f88cb04f05138cff71db8970730ba183311280c9
Marco González <gonzalezariasmarcoantonio@gmail.com>
2026-01-26 13:27:09 -0500

    Corrección de creación

--- lib/core/providers/tournament_provider.dart
+++ lib/core/providers/tournament_provider.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:cuarta_ruta_app/core/enums/phases_enum.dart';
+import 'package:cuarta_ruta_app/core/enums/tournament_type_enum.dart';
 import 'package:cuarta_ruta_app/core/services/tournament_storage_base.dart';
 import 'package:cuarta_ruta_app/models/impl/knockout_tournament_model.dart';
+import 'package:cuarta_ruta_app/models/impl/league_tournament_model.dart';
 
 class TournamentProvider extends ChangeNotifier {
+  TournamentTypeEnum _type = TournamentTypeEnum.knockout;
   PhasesEnum _selectedPhase = PhasesEnum.roundOf16;
   bool _hasThirdPlace = false;
   bool _hasWildcard = false;
   int _replicaCount = 1;
   int _pointsDifference = 2;
+
+  // Propiedades de Liga
+  int _participantCount = 8;
+  int _battlesPerParticipant = 1;
+  bool _extraPlayer = false;
+  int _roundsPerBattle = 1;
 
   final Map<PhasesEnum, int> _roundsConfig = {};
 
@@ -16,16 +25,21 @@ class TournamentProvider extends ChangeNotifier {
     _initializeDefaultRounds();
   }
 
+  // Getters
+  TournamentTypeEnum get type => _type;
   PhasesEnum get selectedPhase => _selectedPhase;
   bool get hasThirdPlace => _hasThirdPlace;
   bool get hasWildcard => _hasWildcard;
   int get replicaCount => _replicaCount;
   int get pointsDifference => _pointsDifference;
+  int get participantCount => _participantCount;
+  int get battlesPerParticipant => _battlesPerParticipant;
+  bool get extraPlayer => _extraPlayer;
+  int get roundsPerBattle => _roundsPerBattle;
   Map<PhasesEnum, int> get roundsConfig => _roundsConfig;
 
   void _initializeDefaultRounds() {
     final phases = PhaseDisplay.getIterable(_selectedPhase, _hasThirdPlace);
-
     _roundsConfig.clear();
     for (final phase in phases) {
       _roundsConfig.putIfAbsent(phase, () => 1);
@@ -33,17 +47,27 @@ class TournamentProvider extends ChangeNotifier {
   }
 
   void updateSettings({
+    TournamentTypeEnum? type,
     PhasesEnum? phase,
     bool? thirdPlace,
     bool? wildcard,
     int? replicas,
     int? pointsDiff,
+    int? participants,
+    int? battles,
+    bool? extra,
+    int? rounds,
   }) {
+    _type = type ?? _type;
     _selectedPhase = phase ?? _selectedPhase;
     _hasThirdPlace = thirdPlace ?? _hasThirdPlace;
     _hasWildcard = wildcard ?? _hasWildcard;
     _replicaCount = replicas ?? _replicaCount;
     _pointsDifference = pointsDiff ?? _pointsDifference;
+    _participantCount = participants ?? _participantCount;
+    _battlesPerParticipant = battles ?? _battlesPerParticipant;
+    _extraPlayer = extra ?? _extraPlayer;
+    _roundsPerBattle = rounds ?? _roundsPerBattle;
 
     _initializeDefaultRounds();
     notifyListeners();
@@ -59,25 +83,38 @@ class TournamentProvider extends ChangeNotifier {
     String name,
     TournamentStorageBase storage,
   ) async {
-    final validPhases = PhaseDisplay.getIterable(
-      _selectedPhase,
-      _hasThirdPlace,
-    );
+    if (_type == TournamentTypeEnum.knockout) {
+      final validPhases = PhaseDisplay.getIterable(
+        _selectedPhase,
+        _hasThirdPlace,
+      );
+      final filteredRoundsConfig = {
+        for (final phase in validPhases) phase: _roundsConfig[phase] ?? 1,
+      };
 
-    final filteredRoundsConfig = {
-      for (final phase in validPhases) phase: _roundsConfig[phase] ?? 1,
-    };
-
-    final tournament = KnockoutTournamentModel(
-      name: name,
-      pointsDifference: _pointsDifference,
-      replicaCount: _replicaCount,
-      startPhase: _selectedPhase,
-      hasThirdPlace: _hasThirdPlace,
-      hasWildcard: _hasWildcard,
-      roundsConfig: filteredRoundsConfig,
-    );
-
-    await storage.create(tournament);
+      await storage.create(
+        KnockoutTournamentModel(
+          name: name,
+          pointsDifference: _pointsDifference,
+          replicaCount: _replicaCount,
+          startPhase: _selectedPhase,
+          hasThirdPlace: _hasThirdPlace,
+          hasWildcard: _hasWildcard,
+          roundsConfig: filteredRoundsConfig,
+        ),
+      );
+    } else {
+      await storage.create(
+        LeagueTournamentModel(
+          name: name,
+          pointsDifference: _pointsDifference,
+          replicaCount: _replicaCount,
+          participantCount: _participantCount,
+          battlesPerParticipant: _battlesPerParticipant,
+          extraPlayer: _extraPlayer,
+          roundsPerBattle: _roundsPerBattle,
+        ),
+      );
+    }
   }
 }
--- lib/screens/tournament/creation/widgets/general_settings_widget.dart
+++ lib/screens/tournament/creation/widgets/general_settings_widget.dart
@@ -3,8 +3,10 @@ import 'package:provider/provider.dart';
 import 'package:cuarta_ruta_app/core/providers/tournament_provider.dart';
 import 'package:cuarta_ruta_app/core/utils/responsive_util.dart';
 import 'package:cuarta_ruta_app/core/enums/phases_enum.dart';
+import 'package:cuarta_ruta_app/core/enums/tournament_type_enum.dart';
 import 'package:cuarta_ruta_app/core/widgets/dropdown_widget.dart';
 import 'package:cuarta_ruta_app/core/widgets/toggle_option_widget.dart';
+import 'package:cuarta_ruta_app/core/widgets/counter_selector_widget.dart';
 
 class GeneralSettingsWidget extends StatelessWidget {
   const GeneralSettingsWidget({super.key});
@@ -12,90 +14,128 @@ class GeneralSettingsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = context.watch<TournamentProvider>();
+    final style = Theme.of(context).textTheme.labelLarge;
 
     return Container(
       padding: EdgeInsets.symmetric(horizontal: context.res.wp(10)),
       child: Column(
         children: [
-          _buildTitle(context),
+          _buildTitle(context, 'Ajustes Generales'),
           SizedBox(height: context.res.hp(1)),
-          _buildPhaseDropdown(context, provider),
+          _buildTypeDropdown(context, provider),
           SizedBox(height: context.res.hp(1)),
-          _buildOptions(context, provider),
-          SizedBox(height: context.res.hp(1)),
+          _buildCommonSettings(context, provider, style),
+          if (provider.type == TournamentTypeEnum.knockout)
+            _buildKnockoutSettings(context, provider, style)
+          else
+            _buildLeagueSettings(context, provider, style),
         ],
       ),
     );
   }
 
-  Widget _buildTitle(BuildContext context) {
-    return Text(
-      'Ajustes Generales',
-      textAlign: TextAlign.center,
-      style: Theme.of(context).textTheme.bodyMedium,
-    );
+  Widget _buildTitle(BuildContext context, String text) {
+    return Text(text, textAlign: TextAlign.center, style: Theme.of(context).textTheme.bodyMedium);
   }
 
-  Widget _buildPhaseDropdown(
-    BuildContext context,
-    TournamentProvider provider,
-  ) {
-    return DropdownWidget<PhasesEnum>(
-      label: 'Fase Inicial',
-      value: provider.selectedPhase,
-      items: const [PhasesEnum.roundOf16, PhasesEnum.quarterFinals, PhasesEnum.semifinals],
-      itemLabelBuilder: (phase) => phase.displayName,
-      onChanged: (val) => _updatePhase(provider, val),
+  Widget _buildTypeDropdown(BuildContext context, TournamentProvider provider) {
+    return DropdownWidget<TournamentTypeEnum>(
+      label: 'Tipo de Torneo',
+      value: provider.type,
+      items: TournamentTypeEnum.values,
+      itemLabelBuilder: (type) => type == TournamentTypeEnum.knockout ? 'Eliminatorias' : 'Liga',
+      onChanged: (val) => provider.updateSettings(type: val),
       textStyle: Theme.of(context).textTheme.labelLarge,
     );
   }
 
-  void _updatePhase(TournamentProvider provider, PhasesEnum? val) {
-    if (val != null) {
-      provider.updateSettings(val, provider.hasThirdPlace, provider.hasReplica);
-    }
-  }
-
-  Widget _buildOptions(BuildContext context, TournamentProvider provider) {
-    final style = Theme.of(context).textTheme.labelLarge;
+  Widget _buildCommonSettings(BuildContext context, TournamentProvider provider, TextStyle? style) {
     return Column(
       children: [
-        _buildToggle(
-          '3er Puesto',
-          provider.hasThirdPlace,
-          style,
-          (val) => provider.updateSettings(
-            provider.selectedPhase,
-            val,
-            provider.hasReplica,
-          ),
+        CounterSelectorWidget(
+          label: 'Dif. Puntos',
+          count: provider.pointsDifference,
+          onIncrement: () => provider.updateSettings(pointsDiff: provider.pointsDifference + 1),
+          onDecrement: () => provider.updateSettings(pointsDiff: (provider.pointsDifference - 1).clamp(1, 10)),
+          textStyle: style,
         ),
         SizedBox(height: context.res.hp(1)),
-        _buildToggle(
-          'Permitir Réplica',
-          provider.hasReplica,
-          style,
-          (val) => provider.updateSettings(
-            provider.selectedPhase,
-            provider.hasThirdPlace,
-            val,
-          ),
+        CounterSelectorWidget(
+          label: 'Réplicas',
+          count: provider.replicaCount,
+          onIncrement: () => provider.updateSettings(replicas: provider.replicaCount + 1),
+          onDecrement: () => provider.updateSettings(replicas: (provider.replicaCount - 1).clamp(0, 5)),
+          textStyle: style,
         ),
       ],
     );
   }
 
-  Widget _buildToggle(
-    String label,
-    bool value,
-    TextStyle? style,
-    ValueChanged<bool> onChanged,
-  ) {
-    return ToggleOptionWidget(
-      label: label,
-      value: value,
-      onChanged: onChanged,
-      textStyle: style,
+  Widget _buildKnockoutSettings(BuildContext context, TournamentProvider provider, TextStyle? style) {
+    return Column(
+      children: [
+        SizedBox(height: context.res.hp(1)),
+        DropdownWidget<PhasesEnum>(
+          label: 'Fase Inicial',
+          value: provider.selectedPhase,
+          items: const [PhasesEnum.roundOf16, PhasesEnum.quarterFinals, PhasesEnum.semifinals],
+          itemLabelBuilder: (phase) => phase.displayName,
+          onChanged: (val) => provider.updateSettings(phase: val),
+          textStyle: style,
+        ),
+        SizedBox(height: context.res.hp(1)),
+        ToggleOptionWidget(
+          label: '3er Puesto',
+          value: provider.hasThirdPlace,
+          onChanged: (val) => provider.updateSettings(thirdPlace: val),
+          textStyle: style,
+        ),
+        SizedBox(height: context.res.hp(1)),
+        ToggleOptionWidget(
+          label: 'Comodín',
+          value: provider.hasWildcard,
+          onChanged: (val) => provider.updateSettings(wildcard: val),
+          textStyle: style,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLeagueSettings(BuildContext context, TournamentProvider provider, TextStyle? style) {
+    return Column(
+      children: [
+        SizedBox(height: context.res.hp(1)),
+        CounterSelectorWidget(
+          label: 'Participantes',
+          count: provider.participantCount,
+          onIncrement: () => provider.updateSettings(participants: provider.participantCount + 1),
+          onDecrement: () => provider.updateSettings(participants: (provider.participantCount - 1).clamp(2, 32)),
+          textStyle: style,
+        ),
+        SizedBox(height: context.res.hp(1)),
+        CounterSelectorWidget(
+          label: 'Batallas x Part.',
+          count: provider.battlesPerParticipant,
+          onIncrement: () => provider.updateSettings(battles: provider.battlesPerParticipant + 1),
+          onDecrement: () => provider.updateSettings(battles: (provider.battlesPerParticipant - 1).clamp(1, 10)),
+          textStyle: style,
+        ),
+        SizedBox(height: context.res.hp(1)),
+        CounterSelectorWidget(
+          label: 'Rondas x Batalla',
+          count: provider.roundsPerBattle,
+          onIncrement: () => provider.updateSettings(rounds: provider.roundsPerBattle + 1),
+          onDecrement: () => provider.updateSettings(rounds: (provider.roundsPerBattle - 1).clamp(1, 5)),
+          textStyle: style,
+        ),
+        SizedBox(height: context.res.hp(1)),
+        ToggleOptionWidget(
+          label: 'Extra Player',
+          value: provider.extraPlayer,
+          onChanged: (val) => provider.updateSettings(extra: val),
+          textStyle: style,
+        ),
+      ],
     );
   }
 }
